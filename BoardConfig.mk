c9b5a5cf70b1324974ba5df3fbe27deee077a1de
RezaAdi0105 <39455862+rezaadi0105@users.noreply.github.com>
2019-01-12 09:17:52 -0800

    add exFAT. NTFS and tag official

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,3 +78,12 @@ TW_INCLUDE_NTFS_3G := true
 TW_IGNORE_MISC_WIPE_DATA := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_SCREEN_BLANK_ON_BOOT := true
+
+# exFAT FS Support
+TW_INCLUDE_FUSE_EXFAT := true
+
+# NTFS Support
+TW_INCLUDE_FUSE_NTFS := true
+
+# Official
+PB_OFFICIAL := true
