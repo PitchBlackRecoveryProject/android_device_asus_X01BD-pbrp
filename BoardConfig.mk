9b137d95dba9e032caa81e8aecd59dff928592a9
Rokib Hasan Sagar <rokibhasansagar2014@outlook.com>
2020-04-19 22:37:42 +0600

    Make repository up-to-date

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The Android Open Source Project
+# Copyright (C) 2020 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,6 +20,8 @@
 # included in a build is to use PRODUCT_PACKAGES in a product
 # definition file).
 #
+
+LOCAL_PATH := device/asus/X01BD
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := sdm660
@@ -42,6 +44,10 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+TARGET_CPU_ABI_LIST_64_BIT := $(TARGET_CPU_ABI)
+TARGET_CPU_ABI_LIST_32_BIT := $(TARGET_2ND_CPU_ABI),$(TARGET_2ND_CPU_ABI2)
+TARGET_CPU_ABI_LIST := $(TARGET_CPU_ABI_LIST_64_BIT),$(TARGET_CPU_ABI_LIST_32_BIT)
+
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
 
@@ -51,7 +57,8 @@ BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
-TARGET_PREBUILT_KERNEL := device/asus/X01BD/Image.gz-dtb
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/Image.gz-dtb
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery.fstab
 
 # Partitions
 BOARD_HAS_LARGE_FILESYSTEM := true
@@ -84,6 +91,3 @@ TW_INCLUDE_FUSE_EXFAT := true
 
 # NTFS Support
 TW_INCLUDE_FUSE_NTFS := true
-
-# Official
-PB_OFFICIAL := true
